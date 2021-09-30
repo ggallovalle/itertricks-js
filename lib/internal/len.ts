0651dee3c8ec6b83ddbd012770c15d5d228e0433
Gerson Gallo <ggallovalle@gmail.com>
2021-09-30 09:56:23 -0600

    feat(internal/len): add more utility functions

--- lib/internal/len.ts
+++ lib/internal/len.ts
@@ -1,10 +1,22 @@
 export type CapacityPorter =
   | {
-      length: number
+      length: number;
     }
-  | { size: number }
+  | { size: number };
 
 export default function len(source: CapacityPorter): number {
   //@ts-expect-error either has size or length
-  return source.length ?? source.size
+  return source.length ?? source.size;
+}
+
+export function first<T>(source: T[]): T {
+  return source[0];
+}
+
+export function last<T>(source: T[]): T {
+  return source[source.length - 1];
+}
+
+export function nth<T>(n: number, source: T[]): T {
+  return source[n - 1];
 }
