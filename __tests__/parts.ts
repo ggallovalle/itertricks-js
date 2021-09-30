164a6146b627e6b4b2b6691811f3bf320aae9e93
Gerson Gallo <ggallovalle@gmail.com>
2021-09-29 21:23:47 -0600

    feat(parts): complete

---
+++ __tests__/parts.ts
@@ -0,0 +1,79 @@
+import { chunked, take, windowed } from "../lib/parts";
+import { range } from "../lib/range";
+import len from "../lib/internal/len";
+
+describe("genetrix.parts", () => {
+  describe("take", () => {
+    test("`n=2` returns 2 elements", () => {
+      const source = range(3);
+      const actual = [...take(2, source)];
+      expect(actual.length).toBe(2);
+      expect(actual).toEqual([0, 1]);
+    });
+
+    test("`n` > `source`.length returns all source", () => {
+      const source = range(3);
+      const actual = [...take(1000, source)];
+      expect(actual.length).toBe(4);
+      expect(actual).toEqual([0, 1, 2, 3]);
+    });
+  });
+
+  describe("chunked", () => {
+    test("`size=2` and source par", () => {
+      const source = range(3);
+      const actual = [...chunked(2, source)];
+      expect(actual.length).toBe(2);
+      expect(actual[0]).toEqual([0, 1]);
+      expect(actual[1]).toEqual([2, 3]);
+    });
+
+    test("`size=2` and source inpar", () => {
+      const source = range(2);
+      const actual = [...chunked(2, source)];
+      expect(actual.length).toBe(2);
+      expect(actual[0]).toEqual([0, 1]);
+      expect(actual[1]).toEqual([2]);
+    });
+
+    test("`size` > `source`.length", () => {
+      const sut = range(2);
+      const res = [...chunked(1000, sut)];
+      expect(res.length).toBe(1);
+      expect(res[0]).toEqual([0, 1, 2]);
+    });
+
+    test("throws when `size` <= 0", () => {
+      const sut = range(2);
+      expect(() => {
+        const res = [...chunked(0, sut)];
+      }).toThrow(RangeError);
+    });
+  });
+
+  describe("windowed", () => {
+    test("`size=3` and `len=5` and default step", () => {
+      const source = range(1, 5);
+      const actual = [...windowed(3, source)];
+      expect(len(actual)).toBe(3);
+      expect(actual[0]).toEqual([1, 2, 3]);
+      expect(actual[len(actual) - 1]).toEqual([3, 4, 5]);
+    });
+
+    test("`opts.partialWindow=true`", () => {
+      const source = range(1, 5);
+      const actual = [...windowed(3, source, { partialWindow: true })];
+      expect(len(actual)).toBe(4);
+      expect(actual[0]).toEqual([1, 2, 3]);
+      expect(actual[len(actual) - 1]).toEqual([4, 5]);
+    });
+
+    test("`opts.step=3`", () => {
+      const source = range(1, 10);
+      const actual = [...windowed(5, source, { step: 3 })];
+      expect(len(actual)).toBe(2);
+      expect(actual[0]).toEqual([1, 2, 3, 4, 5]);
+      expect(actual[1]).toEqual([4, 5, 6, 7, 8]);
+    });
+  });
+});
---
+++ lib/parts.ts
@@ -0,0 +1,80 @@
+/**
+ * Take the first `n` elements out of iterable.
+ *
+ * @template T
+ * @param {number} n
+ * @param {Iterable<T>} source
+ */
+export function* take<T>(n: number, source: Iterable<T>): Generator<T> {
+  let counter = 0;
+  for (const x of source) {
+    counter++;
+    yield x;
+    if (counter === n) {
+      break;
+    }
+  }
+}
+
+/**
+ * Splits the `source` into an array of arrays, each not exiding the given `size`.
+ *
+ * @template T
+ * @param {number} size - the number of elements to take in each list.
+ * @param {Iterable<A>} source
+ * @throws {RangeError} - when `size` <= 0
+ * @yields {T[]} - chunked array of arrays.
+ */
+export function* chunked<T>(size: number, source: Iterable<T>): Generator<T[]> {
+  if (size <= 0) throw new RangeError("`size` MUST be >= 0");
+  let counter = 0;
+  let accumulator: T[] = [];
+  for (const x of source) {
+    counter++;
+    accumulator.push(x);
+    if (counter == size) {
+      yield accumulator;
+      counter = 0;
+      accumulator = [];
+    }
+  }
+  if (accumulator.length !== 0) yield accumulator;
+}
+
+const _windowedDefaultOptions = {
+  step: 1,
+  partialWindow: false,
+};
+/**
+ *
+ * Returns an iterable of snapshots of the window of the given `size` sliding
+ * along the `source` with the given `opts.step`, where each snapshot is an array.
+ *
+ * @template T
+ * @param {number} size - the number of elements to take in each window.
+ * @param {Iterator<A>} source
+ * @param {Object} options
+ * @param {number} [options.step=1] - the number of elements to move the window forward by on each step.
+ * @param {string} [options.partialWindow=false] - controls wether or not to keep partial window.
+ * @yields {T[]}
+ */
+export function* windowed<T>(
+  size: number,
+  source: Iterable<T>,
+  options?: { step?: number; partialWindow?: boolean }
+): Generator<T[]> {
+  options = { ..._windowedDefaultOptions, ...options };
+
+  let counter = 0;
+  let accumulator: T[] = [];
+  for (const x of source) {
+    counter++;
+    accumulator.push(x);
+    if (counter === size) {
+      yield accumulator;
+      counter = counter - (options.step ?? 0);
+      accumulator = accumulator.slice(options.step);
+    }
+  }
+  if (options.partialWindow) yield accumulator;
+}
