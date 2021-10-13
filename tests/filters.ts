5104112025e014976732ce672513a836f9c369d9
Gerson Gallo <ggallovalle@gmail.com>
2021-10-13 12:25:13 -0600

    stle(tests): complete the comments on AAA
    arrange
    act
    assert

--- tests/filters.ts
+++ tests/filters.ts
@@ -5,8 +5,10 @@ import { first, last, len } from "../lib/internal/len";
 
 describe("#filter", () => {
   describe("when every element passes the predicate", () => {
+    // act
     const actual = pipe(range(10), filter(lt(20)), asArray);
 
+    // assert
     test("then the length is the same as the source", () => {
       expect(len(actual)).toBe(11);
       expect(first(actual)).toBe(0);
@@ -15,8 +17,10 @@ describe("#filter", () => {
   });
 
   describe("when not all element passes the predicate", () => {
+    // act
     const actual = pipe(range(10), filter(lt(5)), asArray);
 
+    // assert
     test("then then length is less than the source", () => {
       expect(len(actual)).toBeLessThan(10);
       expect(first(actual)).toBe(0);
@@ -25,8 +29,10 @@ describe("#filter", () => {
   });
 
   describe("when not a single element passes the predicate", () => {
+    // act
     const actual = pipe(range(10), filter(lt(-20)), asArray);
 
+    // assert
     test("then the length is 0", () => {
       expect(len(actual)).toBe(0);
     });
@@ -35,8 +41,10 @@ describe("#filter", () => {
 
 describe("#filterNot", () => {
   describe("when every element passes the condition", () => {
+    // act
     const actual = pipe(range(10), filterNot(lt(0)), asArray);
 
+    // assert
     test("then the length is the same as the source", () => {
       expect(len(actual)).toBe(11);
       expect(first(actual)).toBe(0);
@@ -45,8 +53,10 @@ describe("#filterNot", () => {
   });
 
   describe("when not all element passes the predicate", () => {
+    // act
     const actual = pipe(range(10), filterNot(lt(8)), asArray);
 
+    // assert
     test("then then length is less than the source", () => {
       expect(len(actual)).toBeLessThan(10);
       expect(first(actual)).toBe(8);
@@ -55,8 +65,10 @@ describe("#filterNot", () => {
   });
 
   describe("when not a single element passes the predicate", () => {
+    // act
     const actual = pipe(range(10), filterNot(gt(-1)), asArray);
 
+    // assert
     test("then the length is 0", () => {
       expect(len(actual)).toBe(0);
     });
@@ -65,12 +77,13 @@ describe("#filterNot", () => {
 
 describe("#partition", () => {
   describe("when not a single element match", () => {
+    // act
     const actual = pipe(range(10), partition(gt(10)));
 
+    // assert
     test("then 'right' side would be empty", () => {
       expect(len(actual.right)).toBe(0);
     });
-
     test("and 'left' side would contain all the elements", () => {
       expect(len(actual.left)).toBe(11);
       expect(first(actual.left)).toBe(0);
@@ -79,12 +92,13 @@ describe("#partition", () => {
   });
 
   describe("when matches are distributed", () => {
+    // act
     const actual = pipe(range(10), partition(lt(3)));
 
+    // assert
     test("then the sum of the right and left length is equal to the source length", () => {
       expect(len(actual.right) + len(actual.left)).toBe(11);
     });
-
     test("and elements from one are not included in the other", () => {
       expect(first(actual.right)).not.toBe(first(actual.left));
     });
--- tests/predicates.ts
+++ tests/predicates.ts
@@ -15,62 +15,92 @@ import { eq, lt } from "../lib/internal/mathtools";
 
 describe("#empty", () => {
   test("when source is empty then is true", () => {
+    // act
     const actual = empty([]);
+
+    // assert
     expect(actual).toBe(true);
   });
 
   test("when source is not empty then is false", () => {
+    // act
     const actual = empty(count(0, 2));
+
+    // assert
     expect(actual).toBe(false);
   });
 
   test("when source is null or undefined the is true", () => {
+    // act
     const actual = pipe(newGenerator(null, identity), asArray);
+
+    // assert
     expect(empty(actual)).toBe(true);
   });
 });
 
 describe("#moreThan", () => {
   test("when the source len is less than n then is false", () => {
+    // act
     const actual = pipe(range(3), moreThan(10, lt(10)));
+
+    // assert
     expect(actual).toBe(false);
   });
 
   test("when at least n matches the predicate then is true", () => {
+    // act
     const actual = pipe(range(10), moreThan(3, lt(5)));
+
+    // assert
     expect(actual).toBe(true);
   });
 });
 
 describe("#lessThan", () => {
   test("when the source len is less than n then is true", () => {
+    // act
     const actual = pipe(range(3), lessThan(10, lt(10)));
+
+    // assert
     expect(actual).toBe(true);
   });
 
   test("when more than n matches the predicate then is false", () => {
+    // act
     const actual = pipe(range(10), lessThan(3, lt(5)));
+
+    // assert
     expect(actual).toBe(false);
   });
 });
 
 describe("#all", () => {
   test("when the predicate matches on all elements then is true", () => {
+    // act
     const actual = pipe(range(3), all(lt(10)));
+
+    // assert
     expect(actual).toBe(true);
   });
 });
 
 describe("#none", () => {
   test("when the predicate does not matches on all elements then is true", () => {
+    // act
     const actual = pipe(range(3), none(eq(5)));
+
+    // assert
     expect(actual).toBe(true);
   });
 });
 
 describe("#some", () => {
   test("when the predicate matches on at least one element then is true", () => {
+    // act
     const actual = pipe(range(3), some(eq(0)));
+
+    // assert
     expect(actual).toBe(true);
   });
 });
