92032dfeebcb7a67d00790ae6eadcb6a577d9bd3
Gerson Gallo <ggallovalle@gmail.com>
2021-10-12 19:23:47 -0600

    tests(filter): refactor tests to given/when/then format

--- tests/filters.ts
+++ tests/filters.ts
@@ -3,120 +3,90 @@ import { pipe } from "../lib/internal/functools";
 import { gt, lt } from "../lib/internal/mathtools";
 import { first, last, len } from "../lib/internal/len";
 
-describe("filter", () => {
-  describe("curried", () => {
-    test("every element passes the condition", () => {
-      const actual = pipe(range(10), filter(lt(20)), asArray);
+describe("#filter", () => {
+  describe("when every element passes the predicate", () => {
+    const actual = pipe(range(10), filter(lt(20)), asArray);
+
+    test("then the length is the same as the source", () => {
       expect(len(actual)).toBe(11);
       expect(first(actual)).toBe(0);
       expect(last(actual)).toBe(10);
-    });
-
-    test("x elements passes the condition", () => {
-      const actual = pipe(range(10), filter(lt(5)), asArray);
-      expect(len(actual)).toBe(5);
-      expect(first(actual)).toBe(0);
-      expect(last(actual)).toBe(4);
-    });
-
-    test("not a single element passes the condition", () => {
-      const actual = pipe(range(10), filter(lt(-20)), asArray);
-      expect(len(actual)).toBe(0);
-      expect(first(actual)).toBeUndefined();
-      expect(last(actual)).toBeUndefined();
     });
   });
 
-  describe("non curried", () => {
-    test("every element passes the condition", () => {
-      const actual = pipe(filter(range(10), lt(20)), asArray);
-      expect(len(actual)).toBe(11);
-      expect(first(actual)).toBe(0);
-      expect(last(actual)).toBe(10);
-    });
+  describe("when not all element passes the predicate", () => {
+    const actual = pipe(range(10), filter(lt(5)), asArray);
 
-    test("x elements passes the condition", () => {
-      const actual = pipe(filter(range(10), lt(5)), asArray);
-      expect(len(actual)).toBe(5);
+    test("then then length is less than the source", () => {
+      expect(len(actual)).toBeLessThan(10);
       expect(first(actual)).toBe(0);
       expect(last(actual)).toBe(4);
     });
+  });
 
-    test("not a single element passes the condition", () => {
-      const actual = pipe(filter(range(10), lt(-20)), asArray);
+  describe("when not a single element passes the predicate", () => {
+    const actual = pipe(range(10), filter(lt(-20)), asArray);
+
+    test("then the length is 0", () => {
       expect(len(actual)).toBe(0);
-      expect(first(actual)).toBeUndefined();
-      expect(last(actual)).toBeUndefined();
     });
   });
 });
 
-describe("filterNot", () => {
-  describe("curried", () => {
-    test("every element passes the condition", () => {
-      const actual = pipe(range(10), filterNot(lt(0)), asArray);
+describe("#filterNot", () => {
+  describe("when every element passes the condition", () => {
+    const actual = pipe(range(10), filterNot(lt(0)), asArray);
+
+    test("then the length is the same as the source", () => {
       expect(len(actual)).toBe(11);
       expect(first(actual)).toBe(0);
       expect(last(actual)).toBe(10);
-    });
-
-    test("x elements passes the condition", () => {
-      const actual = pipe(range(10), filterNot(lt(8)), asArray);
-      expect(len(actual)).toBe(3);
-      expect(first(actual)).toBe(8);
-      expect(last(actual)).toBe(10);
-    });
-
-    test("not a single element passes the condition", () => {
-      const actual = pipe(range(10), filterNot(gt(-1)), asArray);
-      expect(len(actual)).toBe(0);
-      expect(first(actual)).toBeUndefined();
-      expect(last(actual)).toBeUndefined();
     });
   });
 
-  describe("non curried", () => {
-    test("every element passes the condition", () => {
-      const actual = pipe(filterNot(range(10), lt(0)), asArray);
-      expect(len(actual)).toBe(11);
-      expect(first(actual)).toBe(0);
-      expect(last(actual)).toBe(10);
-    });
+  describe("when not all element passes the predicate", () => {
+    const actual = pipe(range(10), filterNot(lt(8)), asArray);
 
-    test("x elements passes the condition", () => {
-      const actual = pipe(filterNot(range(10), lt(8)), asArray);
-      expect(len(actual)).toBe(3);
+    test("then then length is less than the source", () => {
+      expect(len(actual)).toBeLessThan(10);
       expect(first(actual)).toBe(8);
       expect(last(actual)).toBe(10);
     });
+  });
 
-    test("not a single element passes the condition", () => {
-      const actual = pipe(filterNot(range(10), gt(-1)), asArray);
+  describe("when not a single element passes the predicate", () => {
+    const actual = pipe(range(10), filterNot(gt(-1)), asArray);
+
+    test("then the length is 0", () => {
       expect(len(actual)).toBe(0);
-      expect(first(actual)).toBeUndefined();
-      expect(last(actual)).toBeUndefined();
     });
   });
 });
 
-describe("partition", () => {
-  describe("when not a single match", () => {
+describe("#partition", () => {
+  describe("when not a single element match", () => {
     const actual = pipe(range(10), partition(gt(10)));
-    test("'right' side would be empty", () => {
+
+    test("then 'right' side would be empty", () => {
       expect(len(actual.right)).toBe(0);
     });
-    test("'left' side would contain all the elements", () => {
+
+    test("and 'left' side would contain all the elements", () => {
       expect(len(actual.left)).toBe(11);
       expect(first(actual.left)).toBe(0);
       expect(last(actual.left)).toBe(10);
     });
   });
 
-  test("when matches are distributed", () => {
+  describe("when matches are distributed", () => {
     const actual = pipe(range(10), partition(lt(3)));
-    expect(len(actual.right)).toBe(3);
-    expect(len(actual.left)).toBe(8);
-    expect(first(actual.right)).toBe(0);
-    expect(first(actual.left)).toBe(3);
+
+    test("then the sum of the right and left length is equal to the source length", () => {
+      expect(len(actual.right) + len(actual.left)).toBe(11);
+    });
+
+    test("and elements from one are not included in the other", () => {
+      expect(first(actual.right)).not.toBe(first(actual.left));
+    });
   });
 });
